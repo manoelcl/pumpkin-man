6f288dfd4e96a41cfbae186f297c310b6b96a030
Manoel Castro <manoelcl@gmail.com>
2023-03-28 10:44:13 +0200

    ADD Ghost shader material with vertex movement trough normals

--- src/components/THREE/Ghost.jsx
+++ src/components/THREE/Ghost.jsx
@@ -1,8 +1,12 @@
-import React, { useRef } from "react";
-import { useEffect } from "react";
-import { useState } from "react";
+import React, { useRef, useEffect, useState, useMemo } from "react";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { AdditiveBlending, AnimationMixer, Color, LoopRepeat } from "three";
+import {
+  AdditiveBlending,
+  AnimationMixer,
+  Color,
+  LoopRepeat,
+  ShaderMaterial,
+} from "three";
 import { useFrame } from "@react-three/fiber";
 import { castShadows } from "../../helpers";
 import enemyColors from "../../resources/enemyColors.json";
@@ -10,6 +14,40 @@ import enemyColors from "../../resources/enemyColors.json";
 export default function Ghost(props) {
   const group = useRef();
   const [model, set] = useState(null);
+
+  const meshRef = useRef();
+
+  // Provisional Shader at least working
+  const material = useMemo(
+    () =>
+      new ShaderMaterial({
+        uniforms: {
+          time: { value: 0 },
+          color: { value: new Color(enemyColors[props.number] || "white") },
+        },
+        vertexShader: `
+          uniform float time;
+          
+          varying vec3 vNormal;
+
+          void main() {
+            vec3 pos = position;
+            vNormal = normal;
+            pos += vNormal * sin(time * 10.0 + position.x * 10.0) * 0.05 * clamp(1.0 - position.y * 1.1, 0.0, 1.0);
+            gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
+          }
+        `,
+        fragmentShader: `
+          uniform vec3 color;
+          varying vec3 vNormal;
+
+          void main() {
+            gl_FragColor = vec4(color, 1.0);
+          }
+        `,
+      }),
+    []
+  );
 
   const mixer = useRef();
   useEffect(() => {
@@ -21,7 +59,8 @@ export default function Ghost(props) {
     console.log(enemyColors[props.number]);
     model.scene.traverse((node) => {
       if (node.isMesh && node.material.name === "baseMat") {
-        node.material.color = new Color(enemyColors[props.number] || "white");
+        node.material = material;
+        //node.material.color = new Color(enemyColors[props.number] || "white");
         node.material.blending = AdditiveBlending;
         node.material.side = 0;
         console.log(node.material);
@@ -48,6 +87,7 @@ export default function Ghost(props) {
 
   useFrame((state, delta) => {
     mixer.current?.update(delta);
+    material.uniforms.time.value = state.clock.elapsedTime;
   });
 
   if (model) {
